fd5e8fd8dad662a169d959569d2a656694297083
Mateo-drr <mateodrr@gmail.com>
2023-05-15 14:45:27 +0200

    idk

--- Ants.py
+++ Ants.py
@@ -5,7 +5,11 @@ Created on Mon May  1 14:37:38 2023
 @author: Mateo-drr
 """
 
-path = 'C:/Users/Mateo-drr/Documents/Mateo/Universidades/Trento/2S/BIAI/'
+path = 'C:/Users/Mateo-drr/Documents/Mateo/Universidades/Trento/2S/BIAI/Project/'
+
+#sum of total stops made is 1213
+#we have 23 lines of buses so
+#making each line do an equal number of stops: 52.739 = 53 stops each line 
 
 from random import Random
 from time import time
@@ -16,7 +20,145 @@ import re
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+from inspyred import benchmarks
+import itertools
+from inspyred import swarm, ec
+from inspyred.ec import selectors
 
+##############################################################################
+
+
+Benchmark = benchmarks.Benchmark
+
+def haversine(lat1, lon1, lat2, lon2):
+    # Convert coordinates to radians
+    lon1, lat1, lon2, lat2 = map(math.radians, [float(lon1), float(lat1), float(lon2), float(lat2)])
+
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+class BusStops(Benchmark):
+    def __init__(self, distances):
+        Benchmark.__init__(self, len(distances))
+        #self.weights = weights
+        self.distances = distances
+        #self.popularity = popularity
+        self.components = [swarm.TrailComponent((i, j), value=(1 / distances[i][j])) for i, j in itertools.permutations(range(len(distances)), 2)]
+        self.bias = 0.5
+        self.bounder = ec.DiscreteBounder([i for i in range(len(distances))])
+        self.maximize = True
+        self._use_ants = True
+
+    def generator(self, random, args):
+        """Return a candidate solution for an evolutionary computation."""
+        locations = [i for i in range(len(self.distances))]
+        random.shuffle(locations)
+        return locations
+    
+    def constructor(self, random, args):
+        """Return a candidate solution for an ant colony optimization."""
+        self._use_ants = True
+        candidate = []
+        max_stops = 53
+        while len(candidate) < max_stops:#len(candidate) < len(self.distances) - 1:
+            # Find feasible components
+            feasible_components = []
+            if len(candidate) == 0:
+                feasible_components = self.components
+            elif len(candidate) == len(self.distances) - 1:
+                first = candidate[0]
+                last = candidate[-1]
+                feasible_components = [c for c in self.components if c.element[0] == last.element[1] and c.element[1] == first.element[0]]
+            else:
+                last = candidate[-1]
+                already_visited = [c.element[0] for c in candidate]
+                already_visited.extend([c.element[1] for c in candidate])
+                already_visited = set(already_visited)
+                feasible_components = [c for c in self.components if c.element[0] == last.element[1] and c.element[1] not in already_visited]
+            if len(feasible_components) == 0:
+                candidate = []
+            else:
+                # Choose a feasible component
+                if random.random() <= self.bias:
+                    next_component = max(feasible_components)
+                else:
+                    next_component = selectors.fitness_proportionate_selection(random, feasible_components, {'num_selected': 1})[0]
+                candidate.append(next_component)
+        return candidate
+    
+    def evaluator(self, candidates, args):
+        """Return the fitness values for the given candidates."""
+        fitness = []
+        if self._use_ants:
+            for candidate in candidates:
+                total = 0
+                for c in candidate:
+                    total += self.distances[c.element[0]][c.element[1]]
+                    #totalp += self.popularity[c.element[0]]
+                last = (candidate[-1].element[1], candidate[0].element[0])
+                total += self.distances[last[0]][last[1]]
+                fitness.append(1 / total)
+    
+        return fitness
+
+#Load the data
+with open(path+'coordf.p', 'rb') as f:
+    coord = pickle.load(f)
+with open(path+'counts.p', 'rb') as f:
+    counts = pickle.load(f)
+    
+#Calculate the euclidean distances between all the stops   
+eucd = np.zeros((400,400))
+for i in range(0,len(coord)):
+    for j in range(0,len(coord)):
+        eucd[i,j] = haversine(*coord[i].split(','),  *coord[j].split(','))
+
+problem = BusStops(eucd)
+prng = Random()
+prng.seed(time()) 
+ac = inspyred.swarm.ACS(prng, problem.components)
+ac.terminator = inspyred.ec.terminators.generation_termination
+final_pop = ac.evolve(generator=problem.constructor, 
+                      evaluator=problem.evaluator, 
+                      bounder=problem.bounder,
+                      maximize=problem.maximize, 
+                      pop_size=50, 
+                      max_generations=100)
+
+best = max(ac.archive)
+print('Best Solution:')
+print('Distance: {0}'.format(1/best.fitness))
+
+lon,lat = 46.07839773659416, 11.126325098487808
+stopsa = [(float(x.split(',')[0]), float(x.split(',')[1])) for x in coord]
+
+# Subtract the southwesternmost coordinates from all stops to get only positive values
+stopsa = [(s[0]-lat, s[1]-lon) for s in stopsa]
+
+
+minx = min(stopsa, key=lambda x: x[0])[0]
+miny = min(stopsa, key=lambda x: x[1])[1]
+
+new_tuples = [(t[0] + np.abs(minx), t[1]+ np.abs(miny)) for t in stopsa]
+fig, ax = plt.subplots(figsize=(10,10))
+ax.scatter([s[1] for s in new_tuples], [s[0] for s in new_tuples], s=5)
+
+# Plot the best route
+best_individual = ac.archive[0]
+best_route = best_individual.candidate
+x = [new_tuples[i][1] for i in range(len(best_route))]
+y = [new_tuples[i][0] for i in range(len(best_route))]
+ax.plot(x, y, 'r')
+
+plt.show()
+
+##############################################################################
+'''
 if True:
     if True:
         prng = Random()
@@ -86,18 +228,6 @@ coord[51] = '46.043855579543646, 11.140421084627958'
 coord[120] = '46.0634250060924, 11.113179201854539'
 
 
-def haversine(lat1, lon1, lat2, lon2):
-    # Convert coordinates to radians
-    lon1, lat1, lon2, lat2 = map(math.radians, [float(lon1), float(lat1), float(lon2), float(lat2)])
-
-    # Haversine formula
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
-    c = 2 * math.asin(math.sqrt(a))
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
-
 # Example usage
 print(haversine(*coord[0].split(','),  *coord[1].split(',')))
 
@@ -114,7 +244,7 @@ for i,stop in enumerate(coord):
         
 #west 46.07839773659416 11.047281547894551 65
 #south 46.00272444937034 11.126325098487808 345
-'''       
+      
         
 G = nx.Graph()
 for i in range(len(coord)):
@@ -122,7 +252,7 @@ for i in range(len(coord)):
         G.add_edge(str(coord[i]), str(coord[j]),
                    weight=haversine(*coord[i].split(','), *coord[j].split(',')))
 
-'''
+
 
 #for i,stop in enumerate(stops):
 for i in range(299,len(stops)):
@@ -144,7 +274,7 @@ with open(path + 'coordf.p', 'wb') as file:
 
 
 # Set the west-most and south-most points as the origin (0, 0)
-'''
+
 west_most_lon, south_most_lat = 46.07839773659416, 11.126325098487808
 
 # Convert all coordinates to Cartesian coordinates
@@ -167,8 +297,9 @@ for cd in coord:
 fig, ax = plt.subplots()
 ax.scatter(x_coords, y_coords, s=5)
 plt.show()
-'''
 
+
+lon,lat = 46.07839773659416, 11.126325098487808
 stopsa = [(float(x.split(',')[0]), float(x.split(',')[1])) for x in coord]
 
 # Subtract the southwesternmost coordinates from all stops to get only positive values
@@ -186,3 +317,14 @@ miny = min(stopsa, key=lambda x: x[1])[1]
 new_tuples = [(t[0] + np.abs(minx), t[1]+ np.abs(miny)) for t in stopsa]
 fig, ax = plt.subplots(figsize=(10,10))
 ax.scatter([s[1] for s in new_tuples], [s[0] for s in new_tuples], s=5)
+
+# Plot the best route
+best_individual = ac.archive[0]
+best_route = best_individual.candidate
+x = [new_tuples[i][1] for i in range(len(best_route))]
+y = [new_tuples[i][0] for i in range(len(best_route))]
+ax.plot(x, y, 'r')
+
+plt.show()
+
+'''
